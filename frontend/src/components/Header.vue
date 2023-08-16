23162d39989b72c6f9bc9615d9d1348e2f8c0cd6
MJDX <edgeeven225@gmail.com>
2023-08-16 09:19:25 +0100

    clean header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,16 +1,12 @@
 <template>
     <header class="bg-gray-800">
         <div class="container mx-auto flex flex-wrap items-center justify-between py-4 px-8">
-            <div class="flex items-center">
+            <div class="flex items-center mx-auto">
                 <a href="#" class="flex items-center text-white">
                     <!-- <img src="logo.png" alt="Logo" class="h-8 w-8 mr-2"> -->
                     <span class="font-bold text-lg">Questionnaire</span>
                 </a>
             </div>
-            <nav class="flex items-center space-x-4 mt-4 md:mt-0">
-                <a href="#" class="text-gray-300 hover:text-white">With Navigation</a>
-                <a href="#" class="text-gray-300 hover:text-white">Without Navigation</a>
-            </nav>
         </div>
     </header>
 </template>
